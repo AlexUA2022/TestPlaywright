0f6752856fcdfc0cbed2e9930b729d78c914e9df
AlexUA2022 <avsaponov@gmail.com>
2024-05-26 12:28:02 +0300

    26/05/2024 12:27

--- page_objects/telescopicLoaderAGRISTARPage.js
+++ page_objects/telescopicLoaderAGRISTARPage.js
@@ -1,4 +1,6 @@
 import HomePage from "./homePage";
+import ModalWindowPreOrdering from "./modalWindowPreOrdering";
+import ModalWindowSuccessfulOrder from "./modalWindowSuccessfulOrder";
 
 class TelescopicLoaderAGRISTARPage {
 	constructor(page) {
@@ -6,13 +8,58 @@ class TelescopicLoaderAGRISTARPage {
 	}
 
  locators = {
-		  getLogo: () => this.page.locator('div').filter({ hasText: /^КаталогВведіть пошукове слово$/ }).getByRole('img').first()		  
+		  getLogo: () => this.page.locator('div').filter({ hasText: /^КаталогВведіть пошукове слово$/ }).getByRole('img').first(),
+		  getMagnifyingGlassIcon: () => this.page.locator('.custom-class-slide > .absolute'),
+		  getModalWindowOpenFoto: () => this.page.locator('div:nth-child(4) > .modal-wrapper > .modal > .modal-body > .wrapper-modal-content'),
+		  getNameProductModalWindow: () => this.page.locator('#modal-root').getByText('Навантажувач телескопічний').nth(3),
+		  getFotoProductModalWindow: () => this.page.locator('#splide10-slide01').getByRole('img', { name: 'Навантажувач телескопічний' }),
+		  getMakePreorderButton: () => this.page.getByRole('button', { name: 'Зробити передзамовлення' }),
+		  getOutOfStockModalWindow: () => this.page.getByText('Передзамовлення товаруЗалиште заявку і наш менеджер зв’яжеться з вами та розпові'),
+		  getFhoneNumberField: () => this.page.getByLabel('Номер телефону+'),
+		  getOutOfStockModalWindowButton: () => this.page.getByRole('button', { name: 'Відправити' }),
+		  getOrderSiccessfulModalWindow: () => this.page.getByText('Замовлення успішне!Очікуйте дзвінка нашого менеджера протягом 5'),
+		  getModalWindowCloseButton: () => this.page.locator('#modal-root').getByRole('button').first(),
+		  getMakePreOrderButton: () => this.page.getByRole('button', { name: 'Зробити передзамовлення' }),
+		  getProductImageBlock: () => this.page.getByText('Навантажувач телескопічний AGRI STAR 37.7 DIECIАртикул: testProduct10', { exact: true }),
+		  getProductImageBig: () => this.page.getByRole('tabpanel', { name: 'of 4' }).getByRole('img'),
+		  getMagnifyingGlassIcon: () => this.page.locator('div').filter({ hasText: /^Навантажувач телескопічний AGRI STAR 37\.7 DIECIАртикул: testProduct10$/ }).getByRole('button').first(),
+		  getProductImageSmall: () => this.page.locator('#splide02-list'),
+		  getProductDescription: () => this.page.getByText('Основні характеристики:Вага (кг):12000Код:testProduct10Виробник:DIECI'),
+		  getProductName: () => this.page.locator('p').filter({ hasText: /^Навантажувач телескопічний AGRI STAR 37\.7 DIECI$/ }),
+		  getProductArticle: () => this.page.getByText('Артикул: testProduct10').nth(1),
+		  getProductPrice: () => this.page.getByText('729000 ₴').first(),
+		  getProductStatus: () => this.page.getByText('під замовлення', { exact: true })
  };
 
 
  async clickLogo() {
 	await this.locators.getLogo().click();
 	return new HomePage(this.page);
+}
+
+async clickMagnifyingGlassIcon() {
+	await this.locators.getMagnifyingGlassIcon().click();
+}
+
+async clickMagnifyingGlassIconPreOrdering() {
+	await this.locators.getMagnifyingGlassIcon().click();
+	return new ModalWindowPreOrdering(this.page);
+}
+
+async clickMakePreorderButton() {
+	await this.locators.getMakePreorderButton().click();
+}
+ async fillFhoneNumberField() {
+	await this.locators.getFhoneNumberField().fill('099 999 89 89');
+ }
+
+ async clickOutOfStockModalWindowButton() {
+	await this.locators.getOutOfStockModalWindowButton().click();
+ }
+
+ async clickMakePreOrderButton() {
+	await this.locators.getMakePreOrderButton().click();
+	return new ModalWindowPreOrdering(this.page);
 }
 
 }
--- tests/test_page_objects/productPageToOrder.spec.js
+++ tests/test_page_objects/productPageToOrder.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 import HomePage from "../../page_objects/homePage.js";
-import {  } from "../../helpers/testDataProductPage.js";
+import {MODAL_WINDOW_NAME_AGRISTAR, MAKE_PREORDER_BUTTON, PRODUCT_INFORMATION_TEXT, PRODUCT_INFORMATION_TEXT_1, PRODUCT_ARTICLE_TEXT_1, PRODUCT_PRICE_TEXT_1, PRODUCT_STATUS_TEXT_1 } from "../../helpers/testDataProductPage.js";
 
 
 test.describe('productListPage.spec.spec', () => {
@@ -11,14 +11,246 @@ test.describe('productListPage.spec.spec', () => {
 
 	});
 
-	// test('TC 04.01.1 Verify that the product page contains a block with the product image', async ({ page }) => {
-	// 	const homePage = new HomePage(page);
+	test.skip('TC 04.01.35.04 Verify that the magnifying glass icon contains a pointer cursor', async ({ page }) => {
+		const homePage = new HomePage(page);
+        const telescopicLoadePage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+		await page.waitForTimeout(2000);
+		await expect(telescopicLoadePage.locators.getMagnifyingGlassIcon()).toHaveCSS('cursor', 'auto');
 
-	// 	const mobilSuper3000Page = await homePage.clickMobilSuper3000();
+	});
 
-	// 	await expect(mobilSuper3000Page.locators.getProductImageBlock()).toBeVisible()
+	test('TC 04.01.35.05  Verify that the modal window opens, the user clicked on the "magnifier"', async ({ page }) => {
+		const homePage = new HomePage(page);
+        const telescopicLoadePage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+		await page.waitForTimeout(2000);
+		await telescopicLoadePage.clickMagnifyingGlassIcon();
+		await expect(telescopicLoadePage.locators.getModalWindowOpenFoto()).toBeAttached();
 
-	// });
+	});
 
+	test('TC 04.01.35.06 Verify that the modal window contains the product name', async ({ page }) => {
+		const homePage = new HomePage(page);
+        const telescopicLoadePage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+		await page.waitForTimeout(2000);
+		await telescopicLoadePage.clickMagnifyingGlassIcon();
+		await expect(telescopicLoadePage.locators.getNameProductModalWindow()).toBeAttached();
+		await expect(telescopicLoadePage.locators.getNameProductModalWindow()).toHaveText(MODAL_WINDOW_NAME_AGRISTAR);
+
+	});
+	test('TC 04.01.35.07 Verify that the modal window contains an image of the product', async ({ page }) => {
+		const homePage = new HomePage(page);
+        const telescopicLoadePage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+		await page.waitForTimeout(2000);
+		await telescopicLoadePage.clickMagnifyingGlassIcon();
+		await expect(telescopicLoadePage.locators.getFotoProductModalWindow()).toBeTruthy();
+
+	});
+
+	test('TC 04.01.23 Verify that the product card with the status "пiд замовлення" has a button "Зробити передзамовлення"', async ({ page }) => {
+		const homePage = new HomePage(page);
+		const telescopicLoadePage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+		await expect(telescopicLoadePage.locators.getMakePreorderButton()).toBeAttached();
+		await expect(telescopicLoadePage.locators.getMakePreorderButton()).toHaveCSS('cursor', 'pointer');
+		await expect(telescopicLoadePage.locators.getMakePreorderButton()).toHaveCSS('border-color', 'rgb(21, 112, 239)');
+		await expect(telescopicLoadePage.locators.getMakePreorderButton()).toHaveText(MAKE_PREORDER_BUTTON);
+
+	});
+
+	test('TC 04.01.24 Verify that the "Передзамовлення товару" dialog box opens, and the user clicks on the "Зробити передзамовлення"button', async ({ page }) => {
+		const homePage = new HomePage(page);
+		const telescopicLoadePage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+		await telescopicLoadePage.clickMakePreorderButton();
+		await expect(telescopicLoadePage.locators.getOutOfStockModalWindow()).toBeAttached();
+
+	});
+
+	test('TC 04.01.25 Verify that the "Передзамовлення товару" dialog box contains the "Номер телефону" field', async ({ page }) => {
+		const homePage = new HomePage(page);
+		const telescopicLoadePage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+		await telescopicLoadePage.clickMakePreorderButton();
+		await expect(telescopicLoadePage.locators.getFhoneNumberField()).toBeVisible();
+	});
+
+	test('TC 04.01.26 Verify that the"Номер телефону" field has verification', async ({ page }) => {
+		const homePage = new HomePage(page);
+		const telescopicLoadePage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+		await telescopicLoadePage.clickMakePreorderButton();
+		await telescopicLoadePage.fillFhoneNumberField();
+		await telescopicLoadePage.clickOutOfStockModalWindowButton();
+		await page.waitForTimeout(2000);
+		const successMessage = await telescopicLoadePage.locators.getOrderSiccessfulModalWindow();
+		await page.waitForTimeout(2000);
+		expect(successMessage).toBeAttached()
+
+	});
+
+	test('TC 04.01.29 Verify that the pop-up window displays the "X" button', async ({ page }) => {
+		const homePage = new HomePage(page);
+		const telescopicLoadePage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+		await telescopicLoadePage.clickMakePreorderButton();
+		await expect(telescopicLoadePage.locators.getModalWindowCloseButton()).toBeVisible();
+	});
+
+	test('TC 04.01.35 Verify that the successful transition to the catalog was made after clicking on the "Перейти до каталогу" button', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		const telescopicLoaderAGRISTARPage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+		const modalWindowPreOrdering = await telescopicLoaderAGRISTARPage.clickMakePreOrderButton();
+		await modalWindowPreOrdering.clickPhoneField();
+		await modalWindowPreOrdering.typePhoneFieldValidData();
+
+		const modalWindowSuccessfulOrder = await modalWindowPreOrdering.clickSendButton();
+		await modalWindowSuccessfulOrder.clickGoToTheCatalogButton();
+
+		await expect(modalWindowSuccessfulOrder.locators.getModalWindow()).not.toBeVisible();
+		await expect(homePage.locators.getProductListPage()).toBeVisible();
+
+	});
+
+	test('TC 04.01.35.01 Verify that the product page contains a block with the product image', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		const telescopicLoaderAGRISTARPage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+
+		await expect(telescopicLoaderAGRISTARPage.locators.getProductImageBlock()).toBeVisible();
+
+	});
+
+	test('TC 04.01.35.02 Verify that the product image block contains an image of the product', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		const telescopicLoaderAGRISTARPage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+
+		await expect(telescopicLoaderAGRISTARPage.locators.getProductImageBig()).toBeVisible();
+
+	});
+
+	test('TC 04.01.35.03 Verify that the product image block contains a button to enlarge the product image (a "magnifying glass with +" icon)', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		const telescopicLoaderAGRISTARPage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+
+		await expect(telescopicLoaderAGRISTARPage.locators.getMagnifyingGlassIcon()).toBeVisible();
+
+	});
+
+	test('TC 04.01.35.08 Verify that the modal window contains the right-scroll button for scrolling images', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		const telescopicLoaderAGRISTARPage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+
+		const modalWindowPreOrdering = await telescopicLoaderAGRISTARPage.clickMagnifyingGlassIconPreOrdering();
+
+		await expect(modalWindowPreOrdering.locators.getButtonNextSlide()).toBeVisible();
+
+	});
+
+	test('TC 04.01.35.09 Verify that the modal window contains the left-scroll button for scrolling images', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		const telescopicLoaderAGRISTARPage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+
+		const modalWindowPreOrdering = await telescopicLoaderAGRISTARPage.clickMagnifyingGlassIconPreOrdering();
+
+		await expect(modalWindowPreOrdering.locators.getButtonPreviousSlide()).toBeVisible();
+
+	});
+
+	test('TC 04.01.35.10 Verify that the modal window contains close(cross) button', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		const telescopicLoaderAGRISTARPage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+
+		const modalWindowPreOrdering = await telescopicLoaderAGRISTARPage.clickMagnifyingGlassIconPreOrdering();
+
+		await expect(modalWindowPreOrdering.locators.getCloseButton()).toBeVisible();
+
+	});
+
+	test('TC 04.01.35.11 Verify that the close(cross) button has a pointer cursor', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		const telescopicLoaderAGRISTARPage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+
+		const modalWindowPreOrdering = await telescopicLoaderAGRISTARPage.clickMagnifyingGlassIconPreOrdering();
+
+		await expect(modalWindowPreOrdering.locators.getCloseButton()).toBeVisible();
+		await expect(modalWindowPreOrdering.locators.getCloseButton()).toHaveCSS('cursor', 'pointer');	
+
+	});
+
+	test('TC 04.01.35.12 Verify that the modal window closes after clicking on the close(cross) button', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		const telescopicLoaderAGRISTARPage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+
+		const modalWindowPreOrdering = await telescopicLoaderAGRISTARPage.clickMagnifyingGlassIconPreOrdering();
+
+		await expect(modalWindowPreOrdering.locators.getCloseButton()).toBeVisible();
+
+		await modalWindowPreOrdering.clickCloseButton();
+
+		await expect(modalWindowPreOrdering.locators.getModalWindow()).not.toBeVisible();
+
+	});
+
+	test('TC 04.01.35.13 Verify that the product image block contains a smaller images of the product', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		const telescopicLoaderAGRISTARPage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+
+		await expect(telescopicLoaderAGRISTARPage.locators.getProductImageSmall()).toBeVisible();
+
+	});
+
+	test('TC 04.01.35.14 Verify that the product page contains the product description header', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		const telescopicLoaderAGRISTARPage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+
+		await expect(telescopicLoaderAGRISTARPage.locators.getProductDescription()).toBeVisible();
+		await expect(telescopicLoaderAGRISTARPage.locators.getProductDescription()).toContainText(PRODUCT_INFORMATION_TEXT);
+
+	});
+
+	test('TC 04.01.35.15 Verify that the block with the product information contains the product name', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		const telescopicLoaderAGRISTARPage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+
+		await expect(telescopicLoaderAGRISTARPage.locators.getProductName()).toBeVisible();
+		await expect(telescopicLoaderAGRISTARPage.locators.getProductName()).toHaveText(PRODUCT_INFORMATION_TEXT_1);
+
+	});
+
+	test('TC 04.01.35.16 Verify that the block with the product information contains the product article', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		const telescopicLoaderAGRISTARPage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+
+		await expect(telescopicLoaderAGRISTARPage.locators.getProductArticle()).toBeVisible();
+		await expect(telescopicLoaderAGRISTARPage.locators.getProductArticle()).toHaveText(PRODUCT_ARTICLE_TEXT_1);
+
+	});
+
+	test('TC 04.01.35.17 Verify that the block with the product information contains the product price', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		const telescopicLoaderAGRISTARPage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+
+		await expect(telescopicLoaderAGRISTARPage.locators.getProductPrice()).toBeVisible();
+		await expect(telescopicLoaderAGRISTARPage.locators.getProductPrice()).toHaveText(PRODUCT_PRICE_TEXT_1);
+
+	});
+
+	test('TC 04.01.35.18 Verify that the block with the product information contains the product status', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		const telescopicLoaderAGRISTARPage = await homePage.clickCardtelescopicLoaderAGRISTAR();
+
+		await expect(telescopicLoaderAGRISTARPage.locators.getProductStatus()).toBeVisible();
+		await expect(telescopicLoaderAGRISTARPage.locators.getProductStatus()).toHaveText(PRODUCT_STATUS_TEXT_1);
+
+	});
 
 })
